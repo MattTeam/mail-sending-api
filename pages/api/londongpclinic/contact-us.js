8138951753e9b8aa8fab1d13afcdafbf53953459
iekmasolutions <iekma.solutions@gmail.com>
2021-01-07 21:56:48 +0530

    SMTP Changed

--- pages/api/londongpclinic/contact-us.js
+++ pages/api/londongpclinic/contact-us.js
@@ -1,4 +1,4 @@
-// The endpoint : https://mail-sending-api.vercel.app/api/londongpclinic-contact
+// The endpoint : https://mail-sending-api.vercel.app/api/londongpclinic/contact-us
 import allowCors from "../../../allow-cors";
 import middleware from"../../../utils/middleware";
 import handler from"../../../utils/handler";
@@ -27,23 +27,23 @@ handler.post(async (req, res) => {
         return res.status(405).json({ errors: errors.array() })
       }
       //using nodemailer library to send emails
-      // const transporter = nodemailer.createTransport({
-      //   service: "gmail",
-      //   auth: {
-      //     user: process.env.USERNAME,
-      //     pass: process.env.PASSWORD, // naturally, replace both with your real credentials or an application-specific password
-      //   },
-      // });
       const transporter = nodemailer.createTransport({
-          // host: "smtp.example.com",
-          host : proces.env.SMTP_HOST,
-          port: proces.env.SMTP_PORT,
-          secure: false, // upgrade later with STARTTLS
-          auth: {
-            user: proces.env.SMTP_USER,
-            pass: proces.env.SMTP_PASS
-          }
+        service: "gmail",
+        auth: {
+          user: process.env.USERNAME,
+          pass: process.env.PASSWORD, // naturally, replace both with your real credentials or an application-specific password
+        },
       });
+      // const transporter = nodemailer.createTransport({
+      //     // host: "smtp.example.com",
+      //     host : proces.env.SMTP_HOST,
+      //     port: proces.env.SMTP_PORT,
+      //     secure: false, // upgrade later with STARTTLS
+      //     auth: {
+      //       user: proces.env.SMTP_USER,
+      //       pass: proces.env.SMTP_PASS
+      //     }
+      // });
       //data.to  rickydubey1986@gmail.com, bablu.developer16@gmail.com
       var mailContent =
         '<!DOCTYPE HTML><head><meta http-equiv="content-type" content="text/html"></head><body><div id="outer" style="width: 80%;margin: 0 auto;margin-top: 10px;"><div id="inner" style="width: 78%;margin: 0 auto;background-color: #fff;font-family: Open Sans,Arial,sans-serif;font-size: 13px;font-weight: normal;line-height: 1.4em;color: #444;margin-top: 10px;"><h3> New contact request using next.js </h3><p>Name : ' +
