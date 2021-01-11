469bb66a2cc3bdfbe772fc56d0ae1bd522d28459
iekmasolutions <iekma.solutions@gmail.com>
2021-01-11 21:39:03 +0530

    db test 3

--- pages/api/londongpclinic/checkdbc.js
+++ pages/api/londongpclinic/checkdbc.js
@@ -6,11 +6,11 @@ import handler from"../../../utils/handler";
 
 handler.get(async (req, res) => { 
     try {
-        // const result = await excuteQuery({query: 'SELECT * FROM new_menus',values: ''});
+        const result = await excuteQuery({query: 'SELECT * FROM new_menus',values: ''});
         // return result[0]; 
-        // console.log("the result : ", result);
+        console.log("the result : ", result);
 
-        res.status(200).json({ status: "OK", result: "result" });
+        res.status(200).json({ status: "OK", result: result });
     } catch (error) {
       console.log("the error : ", error);
 
