5e8e243f454f6c7a19314066a233888f5ff798eb
iekmasolutions <iekma.solutions@gmail.com>
2021-01-12 13:44:39 +0530

    test5

--- pages/api/londongpclinic/checkdbc.js
+++ pages/api/londongpclinic/checkdbc.js
@@ -1,17 +1,17 @@
 import allowCors from "../../../allow-cors";
 
-import excuteQuery from './lib/db';
+import {excuteQuery} from './lib/db';
 import handler from"../../../utils/handler";
  
 
 handler.get(async (req, res) => { 
     console.log("The next req : ",req);
     try {
-        // const result = await excuteQuery({query: 'SELECT * FROM model_has_roles ',values: ''});
+        const result = await excuteQuery({query: 'SELECT * FROM model_has_roles ',values: ''});
         // return result[0]; 
-        console.log("the result : in logs");
+        console.log("the result : in logs" , result);
 
-        res.status(200).json({ status: "OK", result: "test result" });
+        res.status(200).json({ status: "OK", result: result });
     } catch (error) {
       console.log("the error : ", error);
       res.status(400).json({ status: "OK", result: error });
