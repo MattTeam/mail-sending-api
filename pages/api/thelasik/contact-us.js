30906ae7fee8171dfb4f808796f807ba62e7c30c
iekmasolutions <iekma.solutions@gmail.com>
2021-02-02 18:33:21 +0530

    date formated

--- pages/api/thelasik/contact-us.js
+++ pages/api/thelasik/contact-us.js
@@ -59,6 +59,7 @@ handler.post(async (req, res) => {
             rejectUnauthorized: false
         }
       });
+      // Just changed
       //data.to  rickydubey1986@gmail.com, bablu.developer16@gmail.com, matt@dubseo.co.uk swetanshu84@gmail.com, rickydubey1986@gmail.com, bablu.developer16@gmail.com
       var mailContent =  await contactTmep(data) ;
       const mailOptions = {
