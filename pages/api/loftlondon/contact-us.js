edb22b1a76f8c2be06fd02859f30e8d04330f3a7
iekmasolutions <iekma.solutions@gmail.com>
2021-02-26 16:51:49 +0530

    Loft client email added

--- pages/api/loftlondon/contact-us.js
+++ pages/api/loftlondon/contact-us.js
@@ -6,17 +6,7 @@ import initMiddleware from '../../../utils/init-middleware';
 import validateMiddleware from '../../../utils/validate-middleware';
 import { check, validationResult } from 'express-validator'
 import contactTmep from './lib/contact-temp';
-const nodemailer = require("nodemailer");
-// require('dotenv').config();
-// check('message').isIn(['@','#','%','&','*','^']).withMessage('Invalid character in messages !')
-// const validateBody = initMiddleware(
-//   validateMiddleware([
-//       check('name').isString().withMessage('Name should be only string !').isLength({min:4, max: 40}).withMessage('Name length should 4 to 40 charecters !'),
-//       check('email').exists().withMessage("Email is required").isEmail().withMessage("Invalid email address !"), 
-//       check('phone').matches(/^(\+\d{1,3}[- ]?)?\d{10}$/, 'g').withMessage("Phone number is invalid !"),
-//       check('message').exists().withMessage('Message is required !')
-//   ], validationResult)
-// )
+const nodemailer = require("nodemailer"); 
 
 const validateBody = initMiddleware(
   validateMiddleware([
@@ -45,8 +35,7 @@ handler.post(async (req, res) => {
       //     pass: process.env.PASSWORD, // naturally, replace both with your real credentials or an application-specific password
       //   },
       // });
-      const transporter = nodemailer.createTransport({
-          // host: "smtp.example.com",
+      const transporter = nodemailer.createTransport({ 
           pool:true,
           host : process.env.SMTP_HOST_LOFT,
           port: process.env.SMTP_PORT,
@@ -64,21 +53,14 @@ handler.post(async (req, res) => {
       var mailContent =  await contactTmep(data) ;
       const mailOptions = {
         from: "info@loft.london",
-        to: 'rickydubey1986@gmail.com, bablu.developer16@gmail.com,swetanshu84@gmail.com',
+        to: 'matt@dubseo.co.uk,info@loft.london, rickydubey1986@gmail.com',
         subject:
           "loft.london | New "+data.from+" from " + data.name,
         html: mailContent,
-      };
-      console.log("SMTP HOST : ",process.env.SMTP_HOST_LOFT);
-      console.log("SMTP USER : ",process.env.SMTP_USER_LOFT);
-      console.log("SMTP PASS : ",process.env.SMTP_PASS_LOFT);
+      }; 
       const result = await transporter.sendMail(mailOptions);
         res.status(200).send({ status: "OK", result: result });
-    } catch (err) {
-      console.log("mail sending failed : ", err.message);
-      console.log("SMTP HOST : ",process.env.SMTP_HOST_EMER);
-      console.log("SMTP USER : ",process.env.SMTP_USER_EMER);
-      console.log("SMTP PASS : ",process.env.SMTP_PASS_EMER);
+    } catch (err) { 
         res.status(500).json({ status: "FAILED", error: err.message });
     } 
 
